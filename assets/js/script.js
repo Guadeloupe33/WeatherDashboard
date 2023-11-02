66fbbef7981a77f98e4a2773c1ce559901d04325
Guadeloupe33 <stevenfrancius@gmail.com>
2023-11-02 07:20:29 -0400

    updated the ai key

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ function initPage() {
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
     // Assigning a unique API to a variable
-    const APIKey = "84b79da5e5d7c92085660485702f4ce8";
+    const APIKey = "9ff2dd03aa5ea0140a3b500ecf444723";
 
     function getWeather(cityName) {
         // Execute a current weather get request from open weather api
